a279fea8b9ca6281c420b76eaa170abb96ae6de6
Adriley Samuel Ribeiro Barbosa <adrileysrb@gmail.com>
2025-08-10 09:38:05 -0300

    chore: tres questoes resolvidas

--- analise_vendas_streams/sell.analytics/src/main/java/com/small/endpoints/VendaResources.java
+++ analise_vendas_streams/sell.analytics/src/main/java/com/small/endpoints/VendaResources.java
@@ -1,6 +1,9 @@
 package com.small.endpoints;
 
+import java.util.Map;
+
 import com.small.repository.VendaRepository;
+import com.small.services.VendaService;
 
 import jakarta.inject.Inject;
 import jakarta.ws.rs.Path;
@@ -14,9 +17,29 @@ public class VendaResources {
     @Inject
     VendaRepository repository;
 
+    @Inject VendaService service;
+
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     public Response listAll() {
         return Response.ok(repository.listAll()).build();
+    }
+    
+    @GET @Produces(MediaType.APPLICATION_JSON)
+    @Path("/question01")
+    public Response question01() {
+        return Response.ok(service.question01()).build();
+    }
+    
+    @GET @Produces(MediaType.APPLICATION_JSON)
+    @Path("/question02")
+    public Response question02() {
+        return Response.ok(service.question02()).build();
+    }
+
+    @GET @Produces(MediaType.APPLICATION_JSON)
+    @Path("/question03")
+    public Response question03() {
+        return Response.ok(Map.of("nome", service.question03())).build();
     }
 }
---
+++ analise_vendas_streams/sell.analytics/src/main/java/com/small/services/VendaService.java
@@ -0,0 +1,70 @@
+package com.small.services;
+
+import java.util.List;
+import java.util.Map;
+import java.util.LinkedHashMap;
+import java.util.stream.Collectors;
+import java.time.LocalDate;
+import java.util.Comparator;
+import java.util.IntSummaryStatistics;
+
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+
+import com.small.model.Venda;
+import com.small.repository.VendaRepository;
+
+@ApplicationScoped
+public class VendaService {
+
+    @Inject
+    private VendaRepository repository;
+
+    public long question01() {
+        List<Venda> allSellings = repository.listAll();
+
+        IntSummaryStatistics summaryData = allSellings.stream()
+                .filter(sell -> sell.getData().getYear() == LocalDate.now().getYear())
+                .mapToInt(sell -> sell.getQuantidade())
+                .summaryStatistics();
+
+        System.out.println("Quantidade de itens somados na questão 01: " + summaryData.getCount() + "\n");
+
+        return summaryData.getSum();
+    }
+
+    public List<Venda> question02() {
+        List<Venda> allSellings = repository.listAll();
+
+        List<Venda> result = allSellings.stream()
+                .distinct()
+                .sorted(Comparator.comparing(Venda::getProduto))
+                .toList();
+
+        return result;
+    }
+
+    public String question03() {
+        List<Venda> allSellings = repository.listAll();
+
+        Map<String, Integer> groupingByResult = allSellings.stream()
+                .collect(
+                        Collectors.groupingBy(
+                                Venda::getVendedor,
+                                Collectors.summingInt(Venda::getQuantidade)));
+
+        Map<String, Integer> groupingResultOrdered = groupingByResult.entrySet().stream()
+                .sorted(Map.Entry.<String, Integer>comparingByValue().reversed())
+                .collect(
+                        Collectors.toMap(
+                                Map.Entry::getKey,
+                                Map.Entry::getValue,
+                                (e1, e2) -> e1,
+                                LinkedHashMap::new));
+
+        System.out.println();
+        System.out.println(groupingResultOrdered);
+        System.out.println();
+        return groupingResultOrdered.entrySet().iterator().next().getKey();
+    }
+}
